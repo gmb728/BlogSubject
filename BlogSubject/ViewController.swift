d6719b10eb2a3e1c9320f4d2a1cef2fa7a49ab93
Sophia <gmb728@gmail.com>
2020-02-05 16:02:36 +0800

    Initial Commit

---
+++ BlogSubject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlogSubject
+//
+//  Created by Chang Sophia on 2/5/20.
+//  Copyright © 2020 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
